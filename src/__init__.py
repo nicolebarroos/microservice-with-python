378019df7ff6a6840292833a36040370ab58939b
Nicolenewsoft <barrosnicole094@gmail.com>
2021-10-20 16:17:30 -0300

    docker config

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
@@ -5,7 +7,8 @@ app = Flask(__name__)
 
 api = Api(app)
 
-app.config.from_object('src.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 class Ping(Resource):
